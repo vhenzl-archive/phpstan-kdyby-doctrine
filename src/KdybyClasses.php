755032e9924f258ce5931df4a1f134a356a8d999
Vašek Henzl <vasek.henzl@gmail.com>
2017-09-15 10:47:11 +1200

    Add enumeration of FQCN of classes from Kdyby/Doctrine
    Because of conflict in dependency on nette/robot-loader between PHPStan and Kdyby/Doctine (see https://github.com/Kdyby/Doctrine/pull/304),
    Kdyby package can't be installed.

---
+++ src/KdybyClasses.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStanKdybyDoctrine;
+
+class KdybyClasses
+{
+
+	const ENTITY_MANAGER = 'Kdyby\\Doctrine\\EntityManager';
+	const ENTITY_REPOSITORY = 'Kdyby\\Doctrine\\EntityRepository';
+
+}
